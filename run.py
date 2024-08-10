b2dad052e26d8328db19e4952ae7f092716e8a8b
zVNoob <kjen.1342006@gmail.com>
2024-08-10 07:44:19 +0700

    Final code update

--- run.py
+++ run.py
@@ -5,16 +5,12 @@ import dqn
 import env
 import torch
 
-money = input("Enter the money you have: ")
+money = int(input("Enter the money you have: "))
 
 
-Environment = env.Env(100000, 3, 4)
-Network = dqn.DQN_Navie(Environment.size()[0], Environment.size()[1])
+Environment = env.Env(money, 3, 15)
+Network = dqn.DQN(Environment.size()[0], Environment.size()[1])
 
 Network.load_state_dict(torch.load('model.pth'))
 
-done = False
-input = Environment.reset()
-while not done:
-    output = Network(input)
-    input, reward, done = Environment.step(output)
+Network.evaluate(Environment)
